4541c5a9c21d4c757a6e2b8081ba892eab743247
thepavansai <pavansaisriramgara@gmail.com>
2025-03-24 15:53:23 +0530

    Rearrange project directory and push Back-End directory

---
+++ Back-End/HealthNest/src/main/java/com/healthnest/HealthNestApplication.java
@@ -0,0 +1,13 @@
+package com.healthnest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthNestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthNestApplication.class, args);
+	}
+
+}
---
+++ Back-End/HealthNest/src/test/java/com/healthnest/HealthNestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthnest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthNestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
