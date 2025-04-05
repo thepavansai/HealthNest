5530d5fa30d09e762b6a0f4a47b46f27a202eb38
thepavansai <pavansaisriramgara@gmail.com>
2025-04-05 22:21:21 +0530

    user service and controller by gowtham

--- Back-End/HealthNest/src/main/java/com/healthnest/controller/UserController.java
+++ Back-End/HealthNest/src/main/java/com/healthnest/controller/UserController.java
@@ -23,82 +23,72 @@ import com.healthnest.service.UserService;
 @RestController
 @RequestMapping("/users")
 public class UserController {
-	@Autowired 
+	@Autowired
 	UserService userService;
-    @Autowired
-    private ModelMapper modelMapper;
-    @Autowired
-    AppointmentService appointmentservice;
+	@Autowired
+	private ModelMapper modelMapper;
+	@Autowired
+	AppointmentService appointmentservice;
 
 	@PostMapping("/Signup")
-	public ResponseEntity<String> createAccount(@RequestBody UserDTO userdto)
-	{	User user= modelMapper.map(userdto, User.class);
-		if(userService.isUserAlreadyRegistered(user.getEmail()))
-		{
-		 return ResponseEntity.badRequest().body("User already registered!");
-        }
-        userService.createUser(user);
-        return ResponseEntity.ok("User registered successfully!");
+	public ResponseEntity<String> createAccount(@RequestBody UserDTO userdto) {
+		User user = modelMapper.map(userdto, User.class);
+		if (userService.isUserAlreadyRegistered(user.getEmail())) {
+			return ResponseEntity.badRequest().body("User already registered!");
 		}
+		userService.createUser(user);
+		return ResponseEntity.ok("User registered successfully!");
+	}
+
 	@PatchMapping("/editprofile")
 	public ResponseEntity<String> editProfile(@RequestBody User user) {
-	    boolean updated = userService.editProfile(user);
-	    if (updated) {
-	        return ResponseEntity.ok("Profile successfully edited");
-	    } else {
-	        return ResponseEntity.status(404).body("User not found. Failed to edit profile.");
-	    }
+		boolean updated = userService.editProfile(user);
+		if (updated) {
+			return ResponseEntity.ok("Profile successfully edited");
+		} else {
+			return ResponseEntity.status(404).body("User not found. Failed to edit profile.");
+		}
 	}
 
 	@GetMapping("/getupcomingappointments/{id}")
-	public List<Map<String, Object>> getupcomingAppointments1(@PathVariable Integer id)
-	{
+	public List<Map<String, Object>> getupcomingAppointments1(@PathVariable Integer id) {
 		return appointmentservice.getUpcomingAppointments(id);
-		
+
 	}
+
 	@GetMapping("/getcompletedappointments/{id}")
-	public List<Map<String, Object>> getCompletedAppointments(@PathVariable Integer id)
-	{
+	public List<Map<String, Object>> getCompletedAppointments(@PathVariable Integer id) {
 		return appointmentservice.getCompletedAppointments(id);
-		
+
 	}
+
 	@GetMapping("/getcanceledappointments/{id}")
-	public List<Map<String, Object>> getCanceledAppointments(@PathVariable Integer id)
-	{
+	public List<Map<String, Object>> getCanceledAppointments(@PathVariable Integer id) {
 		return appointmentservice.getCancelledAppointments(id);
-		
+
 	}
-	
+
 	@PatchMapping("/cancelappointment/{appointmentId}")
-	public ResponseEntity<String> cancelAppointment(@PathVariable Integer appointmentId)
-	{
+	public ResponseEntity<String> cancelAppointment(@PathVariable Integer appointmentId) {
 		userService.cancleAppointment(appointmentId);
 		return ResponseEntity.ok("sucessfully cancelled Appointment");
 	}
 
 	@PatchMapping("/changepassword/{userid}/{beforepassword}/{changepassword}")
-	public ResponseEntity<String> changepassword(@PathVariable Integer userid,@PathVariable String beforepassword,@PathVariable String changepassword)
-	{
-		if(userService.changePassword(userid, beforepassword, changepassword))
-		{
+	public ResponseEntity<String> changepassword(@PathVariable Integer userid, @PathVariable String beforepassword,
+			@PathVariable String changepassword) {
+		if (userService.changePassword(userid, beforepassword, changepassword)) {
 			return ResponseEntity.ok("sucessfully changed");
-		}
-		else
-		{
+		} else {
 			return ResponseEntity.ok("unsucessfull");
 		}
-		
+
 	}
-	
+
 	@DeleteMapping("/deleteuser/{userId}")
-	public ResponseEntity<String> deleteAccount(@PathVariable Integer userId)
-	{
+	public ResponseEntity<String> deleteAccount(@PathVariable Integer userId) {
 		userService.deleteAccount(userId);
 		return ResponseEntity.ok("Sucessfully deleted user");
 	}
-	
-	
-		
-	}
 
-
+}
