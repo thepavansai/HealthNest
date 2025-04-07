f9e853291aba077b01d795e81bc37895100b2907
thepavansai <pavansaisriramgara@gmail.com>
2025-04-07 15:58:38 +0530

    add more functionality to admin and minor changes in AppointmentShowDTO

--- Back-End/HealthNest/src/main/java/com/healthnest/Repository/UserRepository.java
+++ Back-End/HealthNest/src/main/java/com/healthnest/Repository/UserRepository.java
@@ -1,19 +1,16 @@
 package com.healthnest.Repository;
 import com.healthnest.model.User;
 
-import java.util.List;
+
 import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import com.healthnest.model.Doctor;
-import com.healthnest.model.User;
+
 @Repository
 public interface UserRepository extends CrudRepository<User,Integer> {
 	Optional<User> findByEmail(String email);
-	
-	
 
 }
 
