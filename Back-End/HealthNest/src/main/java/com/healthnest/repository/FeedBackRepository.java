4df7be7636de15cd70692a0f5f672464070b0ce7
thepavansai <pavansaisriramgara@gmail.com>
2025-04-08 10:39:20 +0530

    add FeedBack Functionality and fix package names

---
+++ Back-End/HealthNest/src/main/java/com/healthnest/repository/FeedBackRepository.java
@@ -0,0 +1,19 @@
+package com.healthnest.repository;
+
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.healthnest.dto.FeedBackDTO;
+import com.healthnest.model.FeedBack;
+
+public interface FeedBackRepository extends JpaRepository<FeedBack, Integer> {
+
+    @Query("SELECT new com.healthnest.dto.FeedBackDTO(f.id, d.doctorId, d.doctorName, u.userId, u.name, f.feedback) " +
+           "FROM FeedBack f " +
+           "JOIN f.user u " +
+           "JOIN f.doctor d")
+    List<FeedBackDTO> findAllFeedBacksDTO();
+}
