cfe4f1e0611882f02382c8452356497b58b66be1
thepavansai <pavansaisriramgara@gmail.com>
2025-04-12 19:14:55 +0530

    fix  delete route

--- Front-End/healthnest/src/pages/Admin/ManageDoctors.js
+++ Front-End/healthnest/src/pages/Admin/ManageDoctors.js
@@ -74,7 +74,7 @@ const ManageDoctors = () => {
   const handleDeleteAllDoctors = async () => {
     if (window.confirm('Are you sure you want to delete ALL doctors (active and pending)? This action is irreversible!')) {
       try {
-        await axios.delete('http://localhost:8080/admin/doctors/delete/all');
+        await axios.delete('http://localhost:8080/admin/doctors/delete');
         setDoctors([]);
         setPendingDoctors([]);
         alert('All doctors have been deleted successfully.');
