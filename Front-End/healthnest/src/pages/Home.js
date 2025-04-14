9aa9a7668c505ff37869d71bd28d859248b0fa69
gowthamthotakuri <129211281+gowthamthotakuri@users.noreply.github.com>
2025-04-14 15:39:26 +0530

    added happy patients and other options to bank in payment method

--- Front-End/healthnest/src/pages/Home.js
+++ Front-End/healthnest/src/pages/Home.js
@@ -9,6 +9,8 @@ import DoctorCarousel from "../components/DoctorCarousel";
 import { getHealthTips } from "../services/getHealthTips";
 import { getStatistics } from "../services/getStatistics";
 import ContentCarousel from "../components/ContentCarousel";
+import HappyPatients from "../components/HappyPatients";
+
 const Home = () => {
   const [statistics, setStatistics] = useState({
     doctors: 0,
@@ -148,6 +150,8 @@ const Home = () => {
               )}
             </motion.div>
           </section>
+
+          <HappyPatients />
 
           <section className="doctor-section">
             <DoctorCarousel />
