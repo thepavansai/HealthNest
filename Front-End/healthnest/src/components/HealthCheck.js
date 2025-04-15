a835f9054fd30f3ee77d2c84b197a5afa8b9b823
gowthamthotakuri <129211281+gowthamthotakuri@users.noreply.github.com>
2025-04-15 15:17:48 +0530

    changed home page check health navigation

--- Front-End/healthnest/src/components/HealthCheck.js
+++ Front-End/healthnest/src/components/HealthCheck.js
@@ -19,7 +19,7 @@ const HealthCheck = ({ name, description }) => {
 
     switch (name) {
       case "Health Checkup":
-        navigate("/suggestions");
+        navigate("/user/remedies");
         break;
       case "View Appointments":
         navigate("/login");
