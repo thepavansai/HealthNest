dd8b98ee4afeaa034cd8f2098f6275daf3f5f7db
Janaki-Ram729 <kilumujanakiram@gmail.com>
2025-05-07 11:22:24 +0530

    added library

--- Back-End/HealthNest/src/main/java/com/healthnest/controller/AuthenticationController.java
+++ Back-End/HealthNest/src/main/java/com/healthnest/controller/AuthenticationController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 import com.healthnest.dto.DoctorDTO;
 import com.healthnest.exception.AuthenticationException;
 import com.healthnest.model.Doctor;
